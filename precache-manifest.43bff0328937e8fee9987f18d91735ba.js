a1c597072252772aee858fff514d79b9920c2781
David Bass <david@davidbass.com>
2020-07-02 14:33:22 -0700

    Updates

---
+++ precache-manifest.43bff0328937e8fee9987f18d91735ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a4e32c6492bf6e766e52",
+    "url": "/static/css/main.e026372f.chunk.css"
+  },
+  {
+    "revision": "a4e32c6492bf6e766e52",
+    "url": "/static/js/main.a4e32c64.chunk.js"
+  },
+  {
+    "revision": "194d558d791eab71b333",
+    "url": "/static/js/1.194d558d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "054a5dc2fa036cee0d3dd34f17c099cf",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.af3f5295bd77bc1db917cce1b1f1bf53.js"
+  "/precache-manifest.43bff0328937e8fee9987f18d91735ba.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
